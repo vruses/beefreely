48ebb792c44cd5f511e6a298b1c27c6d2309fb8d
vurses <vurses@qq.com>
2025-08-22 07:18:15 +0800

    init

---
+++ src/popup/App.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+
+  import HelloWorld from '@/components/HelloWorld.svelte'
+</script>
+
+
+<HelloWorld msg='Vite + Svelte + CRXJS' />
+
+<style>
+
+</style>
---
+++ src/sidepanel/App.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+  import HelloWorld from '@/components/HelloWorld.svelte'
+</script>
+
+<HelloWorld msg='Vite + Svelte + CRXJS' />
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .logo.crx:hover {
+    filter: drop-shadow(0 0 2em #f2bae4aa);
+  }
+</style>
