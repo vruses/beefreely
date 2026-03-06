c00f6d9ff4e4464d68c90b96937f21c4e5701781
vurses <vurses@qq.com>
2026-03-06 22:46:41 +0800

    fix: determine login status accurately

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,10 +1,26 @@
 import { random } from 'lodash-es'
 import { domainConfig } from '@/core/config'
-import onRequest from '@/utils/ajax'
+import onRequest, { clearRequestHooks } from '@/utils/ajax'
+import { stop as stopDmIntercept } from '@/utils/websocket/intercept'
 
-// player 判断用户登录的另一种方式，用于尽早执行某些需要登录状态的逻辑
-// biome-ignore lint: <修改document.cookie的唯一方法>
-document.cookie = `DedeUserID=${random(2 ** 53)}; path=/; domain=.bilibili.com`
+// 避免在 iframe 多次执行逻辑
+if (window.top === window.self) {
+  // DedeUserID__ckMd5 字段判断登录
+  if (!document.cookie.match(/DedeUserID__ckMd5=([^;]+)/)?.[1]) {
+    // player 判断用户登录的另一种方式，用于尽早执行某些需要登录状态的逻辑
+    // biome-ignore lint: <修改document.cookie的唯一方法>
+    document.cookie = `DedeUserID=${random(2 ** 53)}; path=/; domain=.bilibili.com`
+  } else {
+    // 已登录，清除可能设置的拦截钩子
+    // 暂时保留 useNav 接口的登录判断
+    queueMicrotask(() => {
+      // 清除所有请求钩子
+      clearRequestHooks()
+      // 移除弹幕 ws 拦截
+      stopDmIntercept()
+    })
+  }
+}
 
 const subdomain = location.host.split('.').shift()
 if (subdomain && !domainConfig.blackList.includes(subdomain)) {
