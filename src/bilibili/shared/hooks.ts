c9ae5bc43b23e0f385861506306b942dbe5159cb
TK7 <64648487+2010TK7@users.noreply.github.com>
2026-03-09 16:03:46 +0800

    复制评论链接 在原逻辑上保留search
    https://github.com/vruses/beefreely/issues/7#issuecomment-4020969803

--- src/bilibili/shared/hooks.ts
+++ src/bilibili/shared/hooks.ts
@@ -42,11 +42,17 @@ export const useReply: RequestFn<'fetch'> = (request) => {
  */
 export const useReplyShareUrl: RequestFn<'fetch'> = (request) => {
   if (!request.url.includes('x/v2/reply/share_reply_material')) return
-  const uri = location.href.split('?').shift()
+  if (location.hostname == 'space.bilibili.com') return
+  const uri = new URL(location)
   const rpid = new URL('https:' + request.url).searchParams.get('rpid')
+  uri.searchParams.set('comment_on', 1)
+  uri.searchParams.set('comment_root_id', rpid)
+  uri.searchParams.set('share_tag', 's_i')
+  uri.hash = `#reply${rpid}`
   request.response = (res) => {
+    if (res.json?.code == 0) return
     res.json = toResult({
-      reply_share_url: `${uri}?comment_on=1&comment_root_id=${rpid}&share_tag=s_i#reply${rpid}`,
+      reply_share_url: uri.toString(),
     })
   }
 }
