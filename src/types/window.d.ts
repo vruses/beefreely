98c9581e5c9c0525497da0d03cd9fc343281ba2e
vurses <vurses@qq.com>
2026-06-24 16:46:19 +0800

    chore: update Window types

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -1,11 +1,29 @@
-export {}
+/** 视频详细信息 */
+export interface VideoDetailView {
+  aid: number
+  bvid: string
+  cid: number
+  title: string
+  pic: string
+  duration: number
+  videos: number
+  tag_name: string
+  /** 视频作者信息 */
+  owner: {
+    face: string
+    mid: number
+    name: string
+  }
+  pages: Array<unknown>
+}
+
 declare global {
   interface Window {
     player?: unknown // Use a more specific type if you know it
-    __INITIAL_STATE__?: unknown
+    __INITIAL_STATE__?: { videoData: VideoDetailView }
     /**
      * 视频页首次加载的视频信息，后续切换视频不再变更
-     * 从__INITIAL_STATE__读取
+     * 从 __INITIAL_STATE__.videoData 读取
      */
     vd?: { aid: number; cid: number }
   }
